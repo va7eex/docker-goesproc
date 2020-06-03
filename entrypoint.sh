0f89a15b70f2d820c87ea960ace0105399f33b10
DR <david@va7eex.ca>
2020-06-03 16:07:58 -0700

    Another typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+#ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 goesproc --config=/usr/local/share/goestools/goesproc-goesr.conf -m packet --subscribe tcp://${IP_GOESRECV}:5004
